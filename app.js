e847cf6fca5b9e0bdb787623c846000f5779194c
WarriorOfLiberation <107781787+WarriorOfLiberation@users.noreply.github.com>
2022-07-21 16:20:21 +0530

    Updated app.js according to date.js

--- app.js
+++ app.js
@@ -1,35 +1,25 @@
 //jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 //const { urlencoded } = require("body-parser");
-var string= [];
-var size=0;
+const day = require(__dirname + "/date.js");
+var string = [];
+var size = 0;
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
-app.get("/", function(req, res){
-var today= new Date();
-var currentDay=today.getDay();
-if(currentDay==6||currentDay==7)
-{
-  var day="weekend";
-  res.render("list", {foo: day, fo:string, size: size});
-}
-else{
-  var day="weekday";
-  res.render("list", {foo: day, fo: string, size: size});
-}
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", function (req, res) {
+  res.render("list", { foo: day.getDay(), fo: string, size: size });
 });
 
-app.post("/", function(req, res){
-   string.push(req.body.Todolistentry);
-   size++;
+app.post("/", function (req, res) {
+  string.push(req.body.Todolistentry);
+  size++;
   console.log(string);
-   res.redirect("/");
+  res.redirect("/");
 })
 
-app.listen(3000, function(){
+app.listen(3000, function () {
   console.log("Server started on port 3000.");
 });
